91ffeeb2ba228678c0bb238fce533ef61ecd8ed5
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-16 03:38:56 +0200

    TODO: docstring

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -1,1 +1,18 @@
 from base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    """
+    def __init__(self, name):
+        """
+        """
+        super().__init__()
+        self.name = name
+
+    def check_length(self, name):
+        if len(self.name) > 50:
+            raise ValueError("The name of the amenity must no exceed 50 characters.")
+        
+    def format_name(self):
+        if not isinstance(self.name, str):
+            raise TypeError("The name of the amenity must be a string")
