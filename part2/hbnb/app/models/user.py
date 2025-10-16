9a0713b36be603e1015fc826d9a0b795cb551992
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-16 12:42:06 +0200

    sauvegarde du travail avant transfer

--- part2/hbnb/app/models/user.py
+++ part2/hbnb/app/models/user.py
@@ -5,6 +5,7 @@ class User(BaseModel):
     """
     User class that inherits from BaseModel.
     """
+    PERMITS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789._'
     def __init__(self, email, password, first_name, last_name, is_admin=False):
         """
         Initialize a User instance with email, password, first name, last name,
@@ -26,5 +27,37 @@ class User(BaseModel):
         self.is_admin = is_admin
 
         #TODO: hash password, print formationg, serialization, check format email, max length first and last name.
+    def validate_name(self):
+        """
+        """
+        if not isinstance(self.first_name, str):
+            raise TypeError("The first name must be an string")
 
+        if len(self.first_name) > 50:
+            raise ValueError("The first name must have a maximum length of 50 characters")
+        
+        if not isinstance(self.last_name_name, str):
+            raise TypeError("The last name must be an string")
+
+        if len(self.last_name) > 50:
+            raise ValueError("The last name must have a maximum length of 50 characters")
+
+    def validate_email(self):
+        """
+        Validate the email format.
+        """
+        if self.email.count("@") != 1:
+            raise ValueError("")
+        
+        recipient, domain = self.email.split("@")
+
+        if not recipient or not domain:
+            raise ValueError("Email must have both recipient and domain parts")
+        
+        
+
+
+    def validate_password(self):
+        """
+        """
 
