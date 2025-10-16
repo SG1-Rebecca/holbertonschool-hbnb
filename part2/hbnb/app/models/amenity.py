5f4263e637f7819117a296658e0f994113b4461e
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-16 14:30:52 +0200

    Removed check_length and format_name methods to be moved to validate_name

--- part2/hbnb/app/models/amenity.py
+++ part2/hbnb/app/models/amenity.py
@@ -9,10 +9,14 @@ class Amenity(BaseModel):
         super().__init__()
         self.name = name
 
-    def check_length(self, name):
-        if len(self.name) > 50:
-            raise ValueError("The name of the amenity must no exceed 50 characters.")
-        
-    def format_name(self):
+    def validate_name(self):
+        """
+        """
         if not isinstance(self.name, str):
             raise TypeError("The name of the amenity must be a string")
+
+        if len(self.name) > 50:
+            raise ValueError("The name of the amenity must no exceed 50 characters.")
+
+        if len(self.name) == 0:
+            raise ValueError("The name of the amenity cannot be empty")
