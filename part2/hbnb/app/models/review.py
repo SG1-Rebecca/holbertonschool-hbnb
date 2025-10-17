394b491763e1a2511ab81eb87149f26301049c14
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-17 11:42:21 +0200

    :memo: Update docstrings

--- part2/hbnb/app/models/review.py
+++ part2/hbnb/app/models/review.py
@@ -1,9 +1,10 @@
-from base_model import BaseModel
-from place import Place
-from user import User
+from app.models.base_model import BaseModel
+from app.models.place import Place
+from app.models.user import User
 
 class Review(BaseModel):
     """
+    Review class that inherits from BaseModel.
     """
     MIN_RATING = 1
     MAX_RATING = 5
@@ -12,10 +13,10 @@ class Review(BaseModel):
         Initialize a Review instance.
 
         Args:
-            text (str):
-            rating (int): 
-            place (Place)
-            user (User)
+            text (str): The content of the review
+            rating (int): The rating given by the user
+            place (Place): The place being reviewed
+            user (User): The user who wrote the review
         """
         super().__init__()
         self.text = text
@@ -25,12 +26,14 @@ class Review(BaseModel):
 
     def validate_text(self):
         """
+        Validate the format of the review text.
         """
         if not isinstance(self.text, str):
             raise ValueError("Text must be a string")
         
     def validate_rating(self):
         """
+        Validate the format and rating of the review.
         """
         if not isinstance(self.rating, int) or self.rating < self.MIN_RATING or self.rating > self.MAX_RATING:
             raise ValueError(f"Rating must be an integer between {self.MIN_RATING} and {self.MAX_RATING}")
@@ -42,7 +45,7 @@ class Review(BaseModel):
         if not isinstance(self.place, Place):
             raise ValueError("place must be a valid instance of Place")
         
-    def validate_user(user):
+    def validate_user(self, user):
         """
         Check if user is a User instance.
         """
