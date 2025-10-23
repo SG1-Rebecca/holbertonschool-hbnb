13258099c9a07f417f6c9ea8b6aea32161c42b57
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-23 02:46:37 +0200

    :hammer: Add PUT endpoint for user update

--- part2/hbnb/app/api/v1/users.py
+++ part2/hbnb/app/api/v1/users.py
@@ -46,3 +46,18 @@ class UserResource(Resource):
         if not user:
             return {'error': 'User not found'}, 404
         return {'id': user.id, 'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email}, 200
+    
+    @api.expect(user_model, validate=True)
+    @api.response(200, 'User updated successfully')
+    @api.response(404, 'User not found')
+    def put(self, user_id):
+        """
+        Update user 
+        """
+        user = facade.get_user(user_id)
+        if not user:
+            return {'error': 'User not found'}, 404
+        
+        user_data = api.payload
+        update_user = facade.update_user(user_id, user_data)
+        return update_user.to_dict(), 200
