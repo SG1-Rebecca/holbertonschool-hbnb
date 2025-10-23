e6e77e179172172c7768f177bbd0b36f96e472c8
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-23 13:19:13 +0200

    :white_check_mark: Add test method verification to ensure it raises errors

--- part2/hbnb/app/test/test_models/test_user.py
+++ part2/hbnb/app/test/test_models/test_user.py
@@ -15,5 +15,37 @@ class TestUser(unittest.TestCase):
         self.assertFalse(user.is_admin)  # Default value
         print("User creation test passed!")
 
+    def test_empty_first_name(self):
+        with self.assertRaises(ValueError):
+            User (
+                first_name="    ",
+                last_name="Doe",
+                email="john.doe@example.com"
+            )
+
+    def test_empty_last_name(self):
+        with self.assertRaises(ValueError):
+            User (
+                first_name="John",
+                last_name="     ",
+                email="john.doe@example.com"
+            )
+
+    def test_empty_email(self):
+        with self.assertRaises(ValueError):
+            User (
+                first_name="John",
+                last_name="Doe",
+                email=""
+            )
+
+    def test_format_email(self):
+        with self.assertRaises(ValueError):
+            User (
+                first_name="John",
+                last_name="Doe",
+                email="johndoeexamplecom"
+            )
+
 if __name__ == "__main__":
     unittest.main()
