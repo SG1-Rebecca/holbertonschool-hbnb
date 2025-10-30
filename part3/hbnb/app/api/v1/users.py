415afc0a6f03ccb5ebe3c0d190e93be33ed7dfbd
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-30 16:21:52 +0100

    :sparkles: feat(api): add password hashing during user registration

--- part3/hbnb/app/api/v1/users.py
+++ part3/hbnb/app/api/v1/users.py
@@ -28,6 +28,9 @@ class UserList(Resource):
         existing_user = facade.get_user_by_email(user_data['email'])
         if existing_user:
             return {'error': 'Email already registered'}, 400
+        
+        pwd_hash = bcrypt.generate_password_hash(user_data['password']).decode('utf-8')
+        user_data['password'] = pwd_hash
 
         new_user = facade.create_user(user_data)
         return {'id': new_user.id, 'first_name': new_user.first_name, 'last_name': new_user.last_name, 'email': new_user.email}, 201
