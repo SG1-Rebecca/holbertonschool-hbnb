fcf69c6bf9f6b36fdd4e4b3ac16dce93bcc2f34f
Rebecca F <rebecca.fsq011@protonmail.com>
2025-10-31 12:18:58 +0100

    :bug: fix(init): move namespace import inside create_app to avoid circular import

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
@@ -10,6 +9,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     bcrypt.init_app(app)
+
+    from app.api.v1.users import api as users_ns
 
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
