4bf4cc40580710179a88481145ae2a0bb2803812
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-05 15:02:53 +0100

    :sparkles: feat(facade): Add amenity methods

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,9 +1,11 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
+from app.models.amenity import Amenity
 
 
 class HBnBFacade:
     def __init__(self):
+        self.amenity_repo = InMemoryRepository()
         self.user_repo = InMemoryRepository()
 
     def create_user(self, user_data):
@@ -33,5 +35,20 @@ class HBnBFacade:
         user.update(user_data)
         return user
 
+# === Amenity ===
+
+    def create_amenity(self, amenity_data):
+        amenity = Amenity(**amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
+
+    def get_amenity(self, amenity_id):
+        return self.amenity_repo.get(amenity_id)
+
+    def get_all_amenities(self):
+        return self.amenity_repo.get_all()
+
+    def update_amenity(self, amenity_id, amenity_data):
+        self.amenity_repo.update(amenity_id, amenity_data)
 
 facade = HBnBFacade()
