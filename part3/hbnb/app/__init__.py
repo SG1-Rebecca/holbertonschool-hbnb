9ae56d05710c87b7593d3d64eb3e4deda48f1013
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-09 00:37:01 +0100

    :wrench: feat(app): Integrate flask_bcrypt extension

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -4,6 +4,9 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as review_ns
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
@@ -16,4 +19,7 @@ def create_app(config_class="config.DevelopmentConfig"):
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(review_ns, path='/api/v1/reviews')
+
+    bcrypt.init_app(app)
+    
     return app
