2ca986aae40c400be52fcefd8adb18f9455ca035
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-09 01:03:20 +0100

    refactor(app): Move namespace imports inside create_app

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,9 +1,5 @@
 from flask import Flask
 from flask_restx import Api
-from app.api.v1.users import api as users_ns
-from app.api.v1.amenities import api as amenities_ns
-from app.api.v1.places import api as places_ns
-from app.api.v1.reviews import api as review_ns
 from flask_bcrypt import Bcrypt
 
 bcrypt = Bcrypt()
@@ -14,12 +10,17 @@ def create_app(config_class="config.DevelopmentConfig"):
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
     app.config.from_object(config_class)
 
+    bcrypt.init_app(app)
+
+    from app.api.v1.users import api as users_ns
+    from app.api.v1.amenities import api as amenities_ns
+    from app.api.v1.places import api as places_ns
+    from app.api.v1.reviews import api as review_ns
+
     # Register the users namespace
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(places_ns, path='/api/v1/places')
     api.add_namespace(review_ns, path='/api/v1/reviews')
 
-    bcrypt.init_app(app)
-    
     return app
