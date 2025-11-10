d2f5a08030a3e1a0247e29d81ac600ab56a30af7
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-10 18:08:05 +0100

    feat(config): reuse SECRET_KEY as JWT_SECRET_KEY for JWT authentication

---
+++ part3/hbnb/config.py
@@ -0,0 +1,17 @@
+import os
+
+
+class Config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'default_secret_key')
+    JWT_SECRET_KEY = SECRET_KEY
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+
+
+config = {
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
+}
