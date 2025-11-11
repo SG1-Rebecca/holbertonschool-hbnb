73aaf1e7304bb2d92d1c3638d66409aae700a3c5
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-11 02:12:14 +0100

    :sparkles: feat(sql): Add Place table

--- part3/hbnb/app/hbnb_table_generation.sql
+++ part3/hbnb/app/hbnb_table_generation.sql
@@ -13,3 +13,16 @@ CREATE TABLE IF NOT EXISTS User (
     password VARCHAR(255) NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE
 );
+
+-- Place table
+
+CREATE TABLE IF NOT EXISTS Place (
+    id CHAR(36) PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(10, 2) NOT NULL,
+    latitude FLOAT,
+    longitude FLOAT,
+    owner_id CHAR(36) NOT NULL,
+    FOREIGN KEY (owner_id) REFERENCES User(id)
+)
