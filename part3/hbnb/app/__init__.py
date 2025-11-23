a2a464668343622b65ccc6d821d322b7f7f980f2
Rebecca F <rebecca.fsq011@protonmail.com>
2025-11-23 23:40:45 +0100

    :sparkles: feat(app): Add bcrypt

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from flask_restx import Api
+from flask_bcrypt import Bcrypt
+
+bcrypt = Bcrypt()
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
+
+    #Initialize extension
+    bcrypt.init_app(app)
 
     # Register the namespace
     from app.api.v1.users import api as users_ns
