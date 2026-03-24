a70684158fd28df1aaa2a7fc4aeca483141041e9
Rebecca F <11508@holbertonstudents.com>
2026-03-25 00:23:17 +0100

    :recycle: refactor(base_model): remove __init__ and use SQLAlchemy column defaults for id and timestamps

--- part3/hbnb/app/models/base_model.py
+++ part3/hbnb/app/models/base_model.py
@@ -1,15 +1,19 @@
+from app import db
 import uuid
-from datetime import datetime
+from datetime import datetime, timezone
 
-class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+
+class BaseModel(db.Model):
+    __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
+
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    created_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc))
+    updated_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
 
     def save(self):
         """Update the updated_at timestamp whenever the object is modified"""
-        self.updated_at = datetime.now()
+        db.session.add(self)
+        db.session.commit()
 
     def update(self, data):
         """Update the attributes of the object based on the provided dictionary"""
