a53d9ea648f0938116faa96133b41218c32ace17
Rebecca F <11508@holbertonstudents.com>
2026-03-25 12:54:28 +0100

    :recycle: restore BaseModel initial code before SQLAlchemy mapping

--- part3/hbnb/app/models/base_model.py
+++ part3/hbnb/app/models/base_model.py
@@ -1,19 +1,15 @@
-from app import db
 import uuid
-from datetime import datetime, timezone
+from datetime import datetime
 
-
-class BaseModel(db.Model):
-    __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
-
-    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
-    created_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc))
-    updated_at = db.Column(db.DateTime(timezone=True), default=datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
+class BaseModel:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def save(self):
         """Update the updated_at timestamp whenever the object is modified"""
-        db.session.add(self)
-        db.session.commit()
+        self.updated_at = datetime.now()
 
     def update(self, data):
         """Update the attributes of the object based on the provided dictionary"""
