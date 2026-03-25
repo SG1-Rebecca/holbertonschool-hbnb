a8e1287aa32e2c8a033ad9da5ad507f67878bb97
Rebecca F <11508@holbertonstudents.com>
2026-03-25 18:00:14 +0100

    :recycle: refactor(facade): switch to SQLAlchemy repository

--- part3/hbnb/app/services/facade.py
+++ part3/hbnb/app/services/facade.py
@@ -1,17 +1,11 @@
-from app.models.user import User
-from app.models.amenity import Amenity
-from app.models.place import Place
-from app.models.review import Review
+from app.models import User
 
-from app.persistence.repository import InMemoryRepository
+from app.persistence.repository import SQLAlchemyRepository
 
 
 class HBnBFacade:
     def __init__(self):
-        self.user_repo = InMemoryRepository()
-        self.amenity_repo = InMemoryRepository()
-        self.place_repo = InMemoryRepository()
-        self.review_repo = InMemoryRepository()
+        self.user_repo = SQLAlchemyRepository(User)
 
     def create_user(self, user_data):
         user = User(**user_data)
