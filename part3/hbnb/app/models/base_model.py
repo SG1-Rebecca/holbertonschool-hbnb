1aa65704fc977da63422f410ec1122f7ff708119
Rebecca F <11508@holbertonstudents.com>
2026-03-26 02:23:15 +0100

    :bug: fix(models): add db.Model to BaseModel

--- part3/hbnb/app/models/base_model.py
+++ part3/hbnb/app/models/base_model.py
@@ -2,7 +2,7 @@ from app import db
 import uuid
 from datetime import datetime
 
-class BaseModel:
+class BaseModel(db.Model):
     __abstract__ = True  # This ensures SQLAlchemy does not create a table for BaseModel
 
     id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
